3aa9b7baa60987dd8e47ac4c46668844e313cc1c
dharmik <gajjardhambha@gmail.com>
2021-11-22 14:43:11 +0530

    calculator with user input

--- main.c
+++ main.c
@@ -12,11 +12,25 @@ int div(int a,int b){
 	return a/b;
 }
 int main(){
+	int no;
 	int a=10,b=8;
+	printf("enter a operation like:\n1 for add\n2 for sub\n3 for mul\n4 for div");
+	scanf("%d",&no);
+switch(no){
+	case 1 :
 	printf("add a and b is : %d",sum(a,b));
+	break;
+	case 2 :
 	printf("sub a and b is : %d",sub(a,b));
+	break;
+	case 3 :
 	printf("mul a and b is : %d",mul(a,b));
+	break;
+	case 4 :
 	printf("div a and b is : %d",div(a,b));
+	break;
+}
+
 	return 0;
 }
 
