c70c3e14f193ab0192d5d902c7fa425726981a0d
dharmik <gajjardhambha@gmail.com>
2021-11-22 14:23:21 +0530

    calculator with add and sub

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
-
-int main(){
-
+int sum(int a, int b){
+	return a+b;
 }
+int sub(int a,int b){
+	return a-b;
+}
+int main(){
+	int a=10,b=8;
+	printf("add a and b is : %d",sum(a,b));
+	printf("add a and b is : %d",sub(a,b));
+	return 0;
+}
+
