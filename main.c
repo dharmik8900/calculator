58740d352d45375d4f5994515a5cd657f815c9c1
dharmik <gajjardhambha@gmail.com>
2021-11-22 14:26:21 +0530

    calculator with all opration

--- main.c
+++ main.c
@@ -5,10 +5,18 @@ int sum(int a, int b){
 int sub(int a,int b){
 	return a-b;
 }
+int mul(int a,int b){
+	return a*b;
+}
+int div(int a,int b){
+	return a/b;
+}
 int main(){
 	int a=10,b=8;
 	printf("add a and b is : %d",sum(a,b));
-	printf("add a and b is : %d",sub(a,b));
+	printf("sub a and b is : %d",sub(a,b));
+	printf("mul a and b is : %d",mul(a,b));
+	printf("div a and b is : %d",div(a,b));
 	return 0;
 }
 
